a1489b881b67a70a65c31bd1dc62df09f54495d2
Giovanni Pires da Silva <giovannipds@gmail.com>
2017-07-12 11:36:30 -0300

    Add a new hero btn and method to see the behavior of pristine without form.reset()

--- src/app/hero-form.component.ts
+++ src/app/hero-form.component.ts
@@ -16,6 +16,7 @@ export class HeroFormComponent {
 
   onSubmit() { this.submitted = true; }
 
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
 }
